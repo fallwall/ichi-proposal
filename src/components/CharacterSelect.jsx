b29bbc778e15eb996411e4d179ead8ca91200a08
XX <fallwall19@gmail.com>
2019-10-24 13:33:32 -0400

    character select hook in effect

--- src/components/CharacterSelect.jsx
+++ src/components/CharacterSelect.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import characters from '../data/characters';
 import "./CharacterSelect.css";
 
 export default function CharacterSelect() {
+  const [char, setChar] = useState(null);
+
   return (
     <div className="character-select">
       <p>Character Select</p>
       {characters.map(c =>
-        <div>
+        <div key={c.id}>
           <div>{c.name}</div>
           <div>{c.role}</div>
+          <div onClick={()=> setChar(c.id)}>SELECT</div>
         </div>
       )}
 
